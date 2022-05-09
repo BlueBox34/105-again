28d1e39966c7c03e4546d93fe32898338252905e
BlueBox <pongsapukanuvech@gmail.com>
2022-05-09 14:16:38 +0700

    W2-05 Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,16 +3,24 @@ import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import Contact from "./ContactComponent";
 import Home from "./HomeComponent";
+
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 import { DISHES } from "../shared/dishes";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
@@ -26,7 +34,13 @@ class Main extends Component {
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
 
     return (
@@ -36,6 +50,7 @@ class Main extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar> */}
+        <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route
@@ -43,16 +58,17 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
 
-        <Header />
-        <Menu
+        {/* <Menu
           dishes={this.state.dishes}
           selectedDish={this.state.selectedDish}
           onClick={(dishId) => this.onDishSelect(dishId)}
         />
-        <Footer />
+
+        <Footer /> */}
       </div>
     );
   }
