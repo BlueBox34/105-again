b3064d4376cb1d49654f77b05a1918b35f1d6439
BlueBox <pongsapukanuvech@gmail.com>
2022-05-09 13:55:17 +0700

    W1-03 Components Part 2

--- src/App.js
+++ src/App.js
@@ -5,23 +5,13 @@ import React, { Component } from "react";
 import Menu from "./components/MenuComponent";
 import { DISHES } from "./shared/dishes";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-    };
-  }
+import Main from "./components/MainComponent";
 
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Main />
       </div>
     );
   }
